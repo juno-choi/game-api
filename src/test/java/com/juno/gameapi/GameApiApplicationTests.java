d2393a49213d08d3ce5d8ffcc6f99d357fb52fbc
juno-choi <ililil9482@naver.com>
2022-04-25 18:20:13 +0900

    project start

---
+++ src/test/java/com/juno/gameapi/GameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juno.gameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
